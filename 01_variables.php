1ca832ef450839a31be666705557a9cc801dc682
koc-gab <kocziangabor2007@gmail.com>
2026-09-03 13:10:04 +0200

    second

---
+++ 01_variables.php
@@ -0,0 +1,21 @@
+<?php
+/*    1. XAMPP (PHP) telepítés, futtatás
+    2. <?php ?> szintaxis
+    3. Változók, konstansok ($vezetekNev, define)
+    4. Adattípusok (string, int, float, bool, array)
+    5. Egyszerű kiírás (echo (print), printf, sprintf, print_r, var_dump)
+*/
+    $i = 10;
+    $f = 3.14;
+    $b = true;
+    $s = "Gábor";
+    var_dump($i);
+    var_dump($f);
+    var_dump($b);
+    var_dump($s);
+    $x = 3;
+    $y = "5";
+    $z = $x + $y;
+    print("$z");
+    $a = [1, 2, 3];
+    var_dump($a);
